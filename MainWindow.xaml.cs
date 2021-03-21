b5cef137ff96d4b66bf70a50860caa7a5f94fe3d
Qunthunnan <qunthunnan@gmail.com>
2021-03-21 09:02:13 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace WPFlesson1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+   
+
+    public partial class MainWindow : Window
+    {
+        static double сalculateString(string expression)
+        {
+            return 0;
+        }
+
+        static bool сheckString(string expression) 
+        {
+           
+            return false;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void input_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Key == Key.Enter)
+            {
+                result.Text = "";
+                result.Text = сalculateString(input.Text).ToString();
+            }
+        }
+    }
+}
---
+++ Models/Num.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLessonCalc.Models
+{
+    class Num
+    {
+        double num = 0;
+        bool doted = false;
+
+        public void addNum(int num)
+        {
+            this.num = Convert.ToDouble(this.num.ToString() + num.ToString());
+        }
+
+        public void addDotNum(int num) 
+        {
+            this.num = this.num = Convert.ToDouble(this.num.ToString() + "." + num.ToString());
+            doted = true;
+        }
+
+        public double getNum()
+        {
+            return num;
+        }
+
+        public bool isDotet() 
+        {
+            return doted;
+        }
+    }
+}
